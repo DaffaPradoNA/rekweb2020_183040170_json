cafea6e93a7c26a5640225153f80b518573e0bea
daffaprado183040170 <74706113+daffaprado183040170@users.noreply.github.com>
2020-11-20 15:09:11 +0700

    Add files via upload

---
+++ rekweb2020_183040170_json/latihan1.php
@@ -0,0 +1,16 @@
+<?php
+
+$mahasiswa = [
+
+      "nama"  => "Sandhika Galih",
+      "nrp"   => "043040023",
+      "email" => "sandhikagalih@unpas.ac.id",
+];
+
+
+
+$data = json_encode($mahasiswa);
+echo $data;
+
+
+?>
